edbcb2da66a17c5f1ff3a8d510c099af67263751
ByeongUk-An <61609264+ByeongUk-An@users.noreply.github.com>
2020-06-16 09:42:27 +0900

    Js:contact scrollIntoView

--- js/main.js
+++ js/main.js
@@ -35,7 +35,22 @@ navbarMenu.addEventListener("click", (event) => {
   if (link == null) {
     return;
   }
-  console.log(event.target.dataset.link);
-  const scrollTo = document.querySelector(link);
-  scrollTo.scrollIntoView({ behavior: "smooth" });
+  // console.log(event.target.dataset.link);
+  // const scrollTo = document.querySelector(link);
+  // scrollTo.scrollIntoView({ behavior: "smooth" });
+  scrollIntoView(link);
 });
+
+// Home Contact 버튼 클릭 이동
+const homeContactBtn = document.querySelector(".home__contact");
+
+homeContactBtn.addEventListener("click", () => {
+  // const scrollTo = document.querySelector("#contact");
+  // scrollTo.scrollIntoView({ behavior: "smooth" });
+  scrollIntoView("#contact");
+});
+
+function scrollIntoView(selector) {
+  const scrollTo = document.querySelector(selector);
+  scrollTo.scrollIntoView({ behavior: "smooth" });
+}
